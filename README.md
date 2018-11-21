eb12763c9bdb1104ed39bbbf195cee123c5c29ce
Hank <hankzimmer7@gmail.com>
2018-11-21 13:17:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arrays
+
+Uses arrays in Java to store user-input values and calculate the average.
---
+++ src/com/hankzimmer/Main.java
@@ -0,0 +1,40 @@
+package com.hankzimmer;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
+    }
+
+    //Method returns an array of values that the user input
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    //Method returns the average of the elements in the array
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        return (double) sum / (double) array.length;
+    }
+}
